ba10d476d073bd5f6cc15646602c3a319c233713
AnimeshNema <34148425+AnimeshNema@users.noreply.github.com>
2018-07-10 17:44:31 -0400

    Update Keypoint_detection.py

--- Keypoint_detection.py
+++ Keypoint_detection.py
@@ -17,9 +17,6 @@ plt.imshow(image)
 # load in a haar cascade classifier for detecting frontal faces
 face_cascade = cv2.CascadeClassifier('detector_architectures/haarcascade_frontalface_default.xml')
 
-# run the detector
-# the output here is an array of detections; the corners of each detection box
-# if necessary, modify these parameters until you successfully identify every face in a given image
 faces = face_cascade.detectMultiScale(image, 1.2, 2)
 
 # make a copy of the original image to plot detections on
@@ -28,7 +25,6 @@ image_with_detections = image.copy()
 # loop over the detected faces, mark the image where each face is found
 for (x,y,w,h) in faces:
     # draw a rectangle around each detected face
-    # you may also need to change the width of the rectangle drawn depending on image resolution
     cv2.rectangle(image_with_detections,(x,y),(x+w,y+h),(255,0,0),3)
 
 fig = plt.figure(figsize=(9,9))
@@ -37,7 +33,7 @@ plt.imshow(image_with_detections)
 #plt.show()
 
 
-# Load your model
+# Load model
 net = Net()
 net.load_state_dict(torch.load('saved_models/mark1_face.pt'))
 
